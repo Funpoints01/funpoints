360e253ad1c3fa5558d33b77471979ed96b20b50
ArneH97 <arne@halcoservices.be>
2026-08-10 22:51:54 +0200

    tag manager

--- src/app/+html.tsx
+++ src/app/+html.tsx
@@ -15,6 +15,9 @@ export default function Root({ children }: PropsWithChildren) {
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
         />
 
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: gtm }} />
+
         {/* PWA / iOS home-screen app */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
@@ -29,10 +32,25 @@ export default function Root({ children }: PropsWithChildren) {
         <ScrollViewStyleReset />
         <style dangerouslySetInnerHTML={{ __html: achtergrond }} />
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKS4M9KK" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+          }}
+        />
+        {children}
+      </body>
     </html>
   );
 }
+
+const gtm = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NKS4M9KK');`;
 
 const achtergrond = `
 html, body { background-color: #FFF8F0; }
